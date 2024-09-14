b58795e41620d9de888b471c0b3c1932740aa773
Alessandro Catucci <graphic.alessandro@gmail.com>
2024-09-14 11:42:03 +0200

    implemented function import & export file js

--- assets/js/counter.js
+++ assets/js/counter.js
@@ -1,47 +1,4 @@
-// Created element
-const main = document.createElement("main");
-const counter = document.createElement("section");
-const minus = document.createElement("div");
-const number = document.createElement("div");
-const plus = document.createElement("div");
-
-// add text
-
-const text = document.createElement("h1");
-text.textContent = "Count everything you need =)";
-text.className = "text";
-main.appendChild(text);
-
-
-// Add class 
-main.className = "container";
-counter.className = "container__counter";
-
-//minus
-minus.className = "button";
-minus.textContent = "-";
-
-// Counter number
-number.className = "number";
-number.textContent = 0;
-
-// plus 
-plus.className = "button";
-plus.textContent = "+";
-
-// append element 
-document.body.appendChild(main);
-main.appendChild(counter);
-counter.appendChild(minus);
-counter.appendChild(number);
-counter.appendChild(plus);
-
-const p = document.createElement("p");
-main.appendChild(p);
-p.style.marginTop = "10px";
-p.textContent = `Sorry :(... you need to add a number`;
-
-const audio = new Audio("../assets/audio/click.mp3");
+import { main, counter, minus, number, plus, text, p, audio } from "./tags.js";
 
 /*
 *
